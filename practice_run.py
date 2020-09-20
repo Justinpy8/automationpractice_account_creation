08b6f78e12d3c46caed44e1f67fbf34369beb251
Justin Ma <justin.ma8419@gmail.com>
2020-09-20 01:06:48 -0400

    [Justin] Account creation functions have been added:email_address_check, title_info, name_info, password_info, date_of_birth.

---
+++ practice_run.py
@@ -0,0 +1,27 @@
+from webdriver_functions import *
+
+url = "http://automationpractice.com/"
+
+launching_website(url)
+
+# username = "jackma@rich.com"
+# password = "Levelup88!"
+#
+# sign_in(username, password)
+
+email = "jj@sweet.com"
+title = "male"
+firstname = "Justin"
+lastname = "Rich"
+password = "1ksdit56"
+date = "13"
+month = 'March'
+year = "1984"
+
+
+
+email_address_check(email)
+title_info(title)
+name_info(firstname, lastname)
+password_info(password)
+date_of_birth(date, month, year)
--- webdriver_functions.py
+++ webdriver_functions.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver import ActionChains
-from selenium.webdriver.support import select
+from selenium.webdriver.support.select import Select
+from selenium.common.exceptions import NoSuchElementException
 import time
+
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(20)
@@ -27,3 +29,78 @@ def sign_in(username, password):
     pw.send_keys(password)
     """Click to sign in"""
     driver.find_element_by_xpath("//button[@id='SubmitLogin']").click()
+
+
+def email_address_check(email):
+    """Entering the account creation page"""
+    sign_in_button = driver.find_element_by_xpath("//a[@class='login']")
+    sign_in_button.click()
+    time.sleep(2)
+    """Entering the email address"""
+    email_address = driver.find_element_by_xpath("//input[@id='email_create']")
+    email_address.send_keys(email)
+    driver.find_element_by_xpath("//button[@id='SubmitCreate']").click()
+    time.sleep(2)
+    """Email address not available error"""
+    try:
+        email_unavailable_error = driver.find_element_by_xpath("//div[@id='create_account_error']")
+        if email_unavailable_error.is_displayed():
+            print("This email address already has an account")
+    except NoSuchElementException as error:
+        print(f"This email address is available for new account creation")
+
+
+def title_info(title):
+    """Title"""
+    male = driver.find_element_by_xpath("//input[@id='id_gender1']")
+    female = driver.find_element_by_xpath("//input[@id='id_gender2']")
+    if title == "male":
+        male.click()
+    if title == "female":
+        female.click()
+    print("Title has been selected")
+
+
+def name_info(firstname, lastname):
+    """first name info"""
+    first_name = driver.find_element_by_xpath("//input[@id='customer_firstname']")
+    first_name.send_keys(firstname)
+    print(f"Entering {firstname} as the first name.")
+
+    """last name info"""
+    last_name = driver.find_element_by_xpath("//input[@id='customer_lastname']")
+    last_name.send_keys(lastname)
+    print(f"Entering {lastname} as the first name.")
+
+
+def password_info(password):
+    """password info"""
+    pass_word = driver.find_element_by_xpath("//input[@id='passwd']")
+    pass_word.send_keys(password)
+    print(f"Entering {password} as the first name.")
+
+
+def date_of_birth(date, month, year,):
+    """date info"""
+    date_dropdown = driver.find_element_by_xpath("//select[@id='days']")
+    date_selection = Select(date_dropdown)
+    date_selection.select_by_value(date)
+    print(f"Selected {date} as the date")
+    time.sleep(1)
+
+    """month info"""
+    month_dropdown = driver.find_element_by_xpath("//select[@id='months']")
+    month_selection = Select(month_dropdown)
+    month_selection.select_by_visible_text(month)
+    print(f"Selected {month} as the month")
+    time.sleep(1)
+
+    """year info"""
+    year_dropdown = driver.find_element_by_xpath("//select[@id='years']")
+    year_selection = Select(year_dropdown)
+    year_selection.select_by_value(year)
+    print(f"Selected {year} as the month")
+    time.sleep(1)
+
+
+
